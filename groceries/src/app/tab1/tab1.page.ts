d47648db3d16bcdef4f73b66dfd0f81d6e83d183
sortiz2 <sortiz2@live.maryville.edu>
2020-11-05 11:56:41 -0500

    Updated

--- groceries/src/app/tab1/tab1.page.ts
+++ groceries/src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,82 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title= "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Bread",
+      quantity: 1
+    },
+    {
+      name: "Banana",
+      quantity: 5
+    },
+    {
+      name: "Sugar",
+      quantity: 3
+    },
+  ];
+
+
+  constructor(public NavCtrl: NavController, public toastController: ToastController, public alertController: AlertController) {}
+
+  async removeItem(item, index){
+    console.log("Removing Item -", item, index);
+    const toast =await this.toastController.create({
+      message: "Removing Item -" + index + "...",
+      duration: 2000
+    });
+    toast.present();
+
+    this.items.splice(index,1);
+  }
+
+  addItem(){
+    console.log("Adding Iem");
+    this.presentAlertPrompt();
+  }
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Add Item',
+      message: 'Please enter item...',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          type: 'text',
+          id: 'name2-id',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Save',
+          handler: item => {
+            console.log('Confirm Save', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
